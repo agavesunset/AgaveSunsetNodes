039446bbba129a55e4bf5d976406b0c73dd4566c
agave_sunset <201904066+agavesunset@users.noreply.github.com>
2025-09-25 11:05:48 +0800

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,23 +1,26 @@
-"""AgaveSunset custom nodes for ComfyUI."""
-
-
-from .calculate_AgaveSunset import CalculateAgaveSunset
-from .type_AgaveSunset import TypeAgaveSunset
+from .type_agavesunset import TypeAgaveSunset
+from .math_agavesunset import MathAgaveSunset
+from .compare_agavesunset import CompareAgaveSunset
+from .maprange_agavesunset import MapRangeAgaveSunset
+from .switch_agavesunset import SwitchAgaveSunset
+from .demux8_agavesunset import Demux8AgaveSunset
 
 NODE_CLASS_MAPPINGS = {
-    "calculate_AgaveSunset": CalculateAgaveSunset,
-    "type_AgaveSunset": TypeAgaveSunset,
+    "TypeAgaveSunset": TypeAgaveSunset,
+    "MathAgaveSunset": MathAgaveSunset,
+    "CompareAgaveSunset": CompareAgaveSunset,
+    "MapRangeAgaveSunset": MapRangeAgaveSunset,
+    "SwitchAgaveSunset": SwitchAgaveSunset,
+    "Demux8AgaveSunset": Demux8AgaveSunset,   
 }
-
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "calculate_AgaveSunset": "calculate_AgaveSunset",
-    "type_AgaveSunset": "type_AgaveSunset",
+    "TypeAgaveSunset": "type_AgaveSunset",
+    "MathAgaveSunset": "math_AgaveSunset",
+    "CompareAgaveSunset": "compare_AgaveSunset",
+    "MapRangeAgaveSunset": "maprange_AgaveSunset",
+    "SwitchAgaveSunset": "switch_AgaveSunset",
+    "Demux8AgaveSunset": "demux8_AgaveSunset",
 }
-
-__all__ = [
-    "NODE_CLASS_MAPPINGS",
-    "NODE_DISPLAY_NAME_MAPPINGS",
-]
 
 
