bdcb094898e437f7d11de80a6ab891f6dbfa5425
agave_sunset <201904066+agavesunset@users.noreply.github.com>
2025-09-25 11:08:57 +0800

    __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from .math_agavesunset import MathAgaveSunset
 from .compare_agavesunset import CompareAgaveSunset
 from .maprange_agavesunset import MapRangeAgaveSunset
 from .switch_agavesunset import SwitchAgaveSunset
-from .demux8_agavesunset import Demux8AgaveSunset
 
 NODE_CLASS_MAPPINGS = {
     "TypeAgaveSunset": TypeAgaveSunset,
@@ -11,7 +10,6 @@ NODE_CLASS_MAPPINGS = {
     "CompareAgaveSunset": CompareAgaveSunset,
     "MapRangeAgaveSunset": MapRangeAgaveSunset,
     "SwitchAgaveSunset": SwitchAgaveSunset,
-    "Demux8AgaveSunset": Demux8AgaveSunset,   
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -20,7 +18,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "CompareAgaveSunset": "compare_AgaveSunset",
     "MapRangeAgaveSunset": "maprange_AgaveSunset",
     "SwitchAgaveSunset": "switch_AgaveSunset",
-    "Demux8AgaveSunset": "demux8_AgaveSunset",
 }
 
 
